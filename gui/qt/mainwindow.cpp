29b9352f1ad4537262d74c4a1a7baf398f049db2
Adrien Bertrand <bertrand.adrien@gmail.com>
2016-11-13 02:22:06 +0100

    [Lua] Initialize the engine

--- gui/qt/mainwindow.cpp
+++ gui/qt/mainwindow.cpp
@@ -1,4 +1,4 @@
-#include "mainwindow.h"
+﻿#include "mainwindow.h"
 #include "ui_mainwindow.h"
 #include "utils.h"
 #include "sendinghandler.h"
@@ -407,6 +407,16 @@ MainWindow::MainWindow(CEmuOpts &cliOpts, QWidget *p) : QMainWindow(p), ui(new U
     m_ports->verticalHeader()->setSectionResizeMode(QHeaderView::Fixed);
     m_breakpoints->verticalHeader()->setSectionResizeMode(QHeaderView::Fixed);
     m_watchpoints->verticalHeader()->setSectionResizeMode(QHeaderView::Fixed);
+
+    // Lua
+    connect(ui->buttonRunLuaScript, &QPushButton::clicked, this, &MainWindow::runLuaScript);
+    connect(ui->buttonLoadLuaScript, &QPushButton::clicked, this, &MainWindow::loadLuaScript);
+    connect(ui->buttonSaveLuaScript, &QPushButton::clicked, this, &MainWindow::saveLuaScript);
+    initLuaThings();
+
+    // Meta Types
+    qRegisterMetaType<uint32_t>("uint32_t");
+    qRegisterMetaType<std::string>("std::string");
 
     setCorner(Qt::BottomLeftCorner, Qt::LeftDockWidgetArea);
     setCorner(Qt::BottomRightCorner, Qt::RightDockWidgetArea);
@@ -2705,3 +2715,36 @@ const char *MainWindow::m_varExtensions[] = {
         "",
         "",
     };
+
+// ------------------------------------------------
+// Lua things
+// ------------------------------------------------
+
+void MainWindow::initLuaThings() {
+    static bool isInited = false;
+    if (!isInited) {
+        lua.set_panic( [](lua_State* L) {
+            const char* message = lua_tostring(L, -1);
+            if (message) {
+                lua_pop(L, 1);
+                fprintf(stderr, "[Lua Panic] %s", message);
+            }
+            return -1;
+        });
+
+        lua.open_libraries(sol::lib::base, sol::lib::package);
+
+        isInited = true;
+    }
+}
+
+void MainWindow::loadLuaScript() {
+
+}
+
+void MainWindow::saveLuaScript() {
+
+}
+
+void MainWindow::runLuaScript() {
+}
